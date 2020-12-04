3423cbd3ee13b7d0d9259e4c7388e7b796042d0a
Hannah Jayne Knight <hannah@tinpot.co.uk>
2020-12-04 15:27:24 +0000

    finish forward selection model

--- forward-selection-model.py
+++ forward-selection-model.py
@@ -10,9 +10,46 @@ from sklearn.linear_model import LogisticRegression
 
 
 data = pd.read_csv('LondonGCSEData.csv') # import csv data here
+
+'''
+
+Here we binarize dependent variable. This is done by creating a column with 1s and 0s
+where 1 = 'good' grade and 0 = 'bad' grade. 
+
+How was this calculated?
+Firstly, the mean attainment 8 score for all schools was found. Schools with an attainment 
+8 higher than the mean got a 'good' score.
+
+'''
+
+meangrade = data['ATT8SCR'].mean()
+sdgrade = data['ATT8SCR'].std()
+data['GCSEgrade'] = np.where(data['ATT8SCR'] >= meangrade , '1', '0')
+
+# get rid of the original 'ATT8SCR' column so that we do not get a linear relationship
+# between this and 'GCSEgrade' which is what we already know!
+drop_nan = data.drop('ATT8SCR', axis=1)
+data = drop_nan
+
 print(data.head())
-#train, other = train_test_split(data, test_size=0.2, random_state=0)
-#validation, test = train_test_split(other, test_size=0.5, random_state=0)
+
+'''
+
+Split the data and separate into respective X and y dataframes.
+
+'''
+
+train, other = train_test_split(data, test_size=0.2, random_state=0)
+validation, test = train_test_split(other, test_size=0.5, random_state=0)
+
+x_train = train.drop(columns=['GCSEgrade'])
+y_train = train['GCSEgrade']
+
+x_val = validation.drop(columns=['GCSEgrade'])
+y_val = validation['GCSEgrade']
+
+x_test = test.drop(columns=['GCSEgrade'])
+y_test = test['GCSEgrade']
 
 '''
 
@@ -60,9 +97,20 @@ Training our model.
 
 '''
 
-#selected_columns = my_fwd_selector(X_train, y_train, X_val, y_val) # find the best variables to use in our model 
+selected_columns = my_fwd_selector(x_train, y_train, x_val, y_val)
+print(selected_columns)
+model = LogisticRegression(C=1e9).fit(x_train[selected_columns], y_train)
 
-#model = LogisticRegression(C=1e9).fit(X_train[selected_columns], y_train) # train the model
+# these are simply arrays of 1s and 0s which is the model's predictions
+y_train_predicted = model.predict(x_train[selected_columns])
+y_val_predicted = model.predict(x_val[selected_columns])
+y_test_predicted = model.predict(x_test[selected_columns])
+
+# note that val accuracy is the accuracy when training the model and
+# test accuracy is the accuracy on testing data set
+print('======= Accuracy  table =======')
+print('Training accuracy is:    {}'.format(accuracy_score(y_train, y_train_predicted)))
+print('Validation accuracy is:  {}'.format(accuracy_score(y_val, y_val_predicted)))
 
 '''
 
