f8a77d9587ba0510fa264a37f5f8edc2fcd53bff
Hannah Jayne Knight <hannah@tinpot.co.uk>
2020-12-05 13:47:18 +0000

    finish forward selection V.2

--- forward-selection-model2.py
+++ forward-selection-model2.py
@@ -1,5 +1,6 @@
-# a foward selection model for our dataset
 
+from itertools import combinations
+import copy
 import warnings
 warnings.filterwarnings("ignore")
 import pandas as pd
@@ -7,7 +8,6 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
-
 
 data = pd.read_csv('LondonGCSEData.csv') # import csv data here
 
@@ -24,11 +24,12 @@ sdgrade = data['ATT8SCR'].std()
 data['GCSEgrade'] = np.where(data['ATT8SCR'] >= meangrade , '1', '0')
 
 # get rid of the original 'ATT8SCR' column so that we do not get a linear relationship
-# between this and 'GCSEgrade' which is what we already know!
+# between this and 'GCSEgrade' which is what we already know! Also drop school identifier
 drop_nan = data.drop('ATT8SCR', axis=1)
 data = drop_nan
+drop_nan = data.drop('URN', axis=1)
+data = drop_nan
 
-print(data.head())
 
 '''
 Split the data and separate into respective X and y dataframes.
@@ -46,23 +47,21 @@ y_val = validation['GCSEgrade']
 x_test = test.drop(columns=['GCSEgrade'])
 y_test = test['GCSEgrade']
 
-'''
 
-The forward selector function.
-
-'''
-
-def my_fwd_selector(X_train, y_train, X_val, y_val):
+def my_fwd_selector(X_train, y_train, X_val, y_val, cols):
     print('=============== Begining forward selection =================')
-    cols = list(X_train.columns) # lists all the headers for each column in X_train (ie the variables)
+    print('Starting with {} as the first attribute'.format(cols[0]))
+    cols_copy = copy.deepcopy(cols)
     best_val_acc = 0
     selected_vars = []
-    while len(cols) > 0:
-        print('Trying {} var models'.format(len(selected_vars) + 1))
+    while len(cols_copy) > 0:
+
+        print('Trying {} var model'.format(len(selected_vars) + 1))
         candidate = None
-        for i in range(len(cols)): # loops through all the variables
-            current_vars = selected_vars.copy() # creates a shallow copy of selected_vars
-            current_vars.append(cols[i]) # adds the current variable we are testing to current_vars
+
+        for i in range(len(cols_copy)):
+            current_vars = selected_vars.copy() 
+            current_vars.append(cols_copy[i]) 
             if len(current_vars) == 1:
                 new_X_train = X_train[current_vars].values.reshape(-1, 1) # convert data to a list of lists as sklearn expects
                 new_X_val = X_val[current_vars].values.reshape(-1, 1)
@@ -73,12 +72,12 @@ def my_fwd_selector(X_train, y_train, X_val, y_val):
             mod = LogisticRegression(C=1e9).fit(new_X_train, y_train) # note that C is the inverse of regularization strength
             val_acc = accuracy_score(y_val, mod.predict(new_X_val))
             if val_acc - best_val_acc > 0.005: # if accuracy has improved...
-                candidate = cols[i] # candidate is the successful variable (ie the one that has been added to the model)
+                candidate = cols_copy[i] # candidate is the successful variable (ie the one that has been added to the model)
                 best_val_acc = val_acc
 
         if candidate is not None:
             selected_vars.append(candidate) # adds candidate to our list of variables
-            cols.remove(candidate) # removes from cols so cannot be selected again
+            cols_copy.remove(candidate) # removes from cols_copy so cannot be selected again
             print('------- Adding {} to the model ---------'.format(candidate))
         else:
             print('------- Accuracy not improved by adding another var -------')
@@ -88,24 +87,33 @@ def my_fwd_selector(X_train, y_train, X_val, y_val):
     return selected_vars
 
 '''
-Training our model.
-'''
 
-selected_columns = my_fwd_selector(x_train, y_train, x_val, y_val)
-print(selected_columns)
-model = LogisticRegression(C=1e9).fit(x_train[selected_columns], y_train)
-
-# these are simply arrays of 1s and 0s which is the model's predictions
-y_train_predicted = model.predict(x_train[selected_columns])
-y_val_predicted = model.predict(x_val[selected_columns])
-y_test_predicted = model.predict(x_test[selected_columns])
-
-# note that val accuracy is the accuracy when training the model and
-# test accuracy is the accuracy on testing data set
-print('======= Accuracy  table =======')
-print('Training accuracy is:    {}'.format(accuracy_score(y_train, y_train_predicted)))
-print('Validation accuracy is:  {}'.format(accuracy_score(y_val, y_val_predicted)))
+This forward selector model loops through every attribute and
+uses the previous forward selector function on each one as the
+starting attribute has a big effect on the final model.
 
 '''
-Finding the precision and recall.
-'''
+
+def fwd_select_every_attribute(X_train, y_train, X_val, y_val):
+    cols = list(X_train.columns) # lists all the attributes
+
+    # for each attribute, makes a new array with the attribute being added first
+    # at the start and the remaining attributes following
+    for i in range(len(cols)):
+        if i == 0:
+            my_fwd_selector(X_train, y_train, X_val, y_val, cols)
+            #print(cols)
+            #print(cols)
+            
+        else:
+            new_cols = copy.deepcopy(cols)
+            var = new_cols[i]
+            new_cols.remove(var)
+            new_cols.insert(0, var)
+            my_fwd_selector(X_train, y_train, X_val, y_val, new_cols)
+            #print(new_cols)
+
+    return 
+
+
+print(fwd_select_every_attribute(x_train, y_train, x_val, y_val))
---
+++ trying-everything-logression-model.py
@@ -0,0 +1,34 @@
+'''
+
+This program aims to find the best Logression Model for a dataset by
+testing every possible combination of attributes in the dataset. To 
+cope with this huge amount of data, it uses the "multiprocessing" 
+package to use all the available processors on the computer it is
+being run on.
+
+'''
+
+from itertools import combinations
+import copy
+import warnings
+warnings.filterwarnings("ignore")
+
+'''
+
+This function lists EVERY possible combination of variables for the model
+
+'''
+
+data = pd.read_csv('LondonGCSEData.csv') # import csv data here
+
+def our_models_to_test(attributes):
+    #group_size = len(attributes)
+    models = []
+    for x in range(len(attributes)):
+        comb = list(combinations(attributes, x))
+        models.append(comb)
+
+    return models
+
+#attributes = data.columns.values.tolist()
+#print(our_models_to_test(attributes))
